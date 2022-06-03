ff3615ac47a50e67b2fc4facf4db79ccfe8eff38
D4rkHeart <leonardo.surdez@gmail.com>
2022-06-03 11:02:38 +0200

    [Feature] Added a dozen dictionary

--- main.py
+++ main.py
@@ -11,3 +11,22 @@ unité = {
     9 : "neuf",
 }
 
+dizaine = {
+    10 : "dix",
+    11 : "onze",
+    12 : "douze", 
+    13 : "treize", 
+    14 : "quatorze", 
+    15 : "quinze", 
+    16 : "seize",
+    20 : "vingt",
+    30 : "trente",
+    40 : "quarante",
+    50 : "cinquante",
+    60 : "soixante",
+    70 : "septante",
+    80 : "quatre-vingt",
+    90 : "nonante"
+}
+
+
