0408e27dc193b19ae07b43f1ba236917342688a2
D4rkHeart <leonardo.surdez@gmail.com>
2022-06-04 11:36:11 +0200

    [Feature] Added hundred and thousand conversion

--- main.py
+++ main.py
@@ -26,33 +26,93 @@ chiffreEnLettre = {
     60 : "soixante",
     70 : "septante",
     80 : "quatre-vingt",
-    90 : "nonante",
-    00 : "cent",
-    000 : "mille",
+    90 : "nonante"
 }
 
-Userinput = input("Veuillez entré un chiffre : ")
-nombreEnChiffre = [int(a) for a in str(Userinput)]
+#Apelle les bonnes fonctions suivant le nombre de chiffre dans le numero d'entrée
+def chiffreAText(nombreBrute):
+    nombreEnChiffre = [int(a) for a in str(nombreBrute)]
+    tailleArray = len(nombreEnChiffre)
+    if tailleArray == 4:
+        return plusPetitQueDixMille(nombreEnChiffre)
+    elif  tailleArray == 3:
+        return plusPetitQueMille(nombreEnChiffre)
+    elif tailleArray == 2:
+        return deuxChiffre(nombreEnChiffre)
+    elif tailleArray == 1:
+        return unChiffre(nombreEnChiffre)
+    else:
+        return "ERROR"
 
-print("`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
-print(f"Voici l'array : {nombreEnChiffre}")
-print("`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`")
+#Génère une phrase à l'aide des sorties en strings des fonctions de conversion
+def  kata_quatre(userInput):
+    i = userInput
+    resultatFinale = str(userInput) + " "
+    while i != 4:
+        resultString = chiffreAText(i)
+        i = len(resultString) 
+        resultatFinale += resultString + " est " + chiffreAText(i) + ", "
+    resultatFinale += "quatre est magique."
+    print(resultatFinale)
 
-def plusPetitQueVingt(nombreEnChiffre) :
-    for x in nombreEnChiffre:
-        print(x,chiffreEnLettre[x])
+#Convertisseur d'unité
+def unChiffre(nombreEnChiffre) :
+    resultat = chiffreEnLettre[nombreEnChiffre[0]]
+    return resultat
 
-plusPetitQueVingt(nombreEnChiffre)
-
+#Convertisseur de dizaine
 def deuxChiffre(nombreEnChiffre):
     dizaine = nombreEnChiffre[0]*10 
     unité = + nombreEnChiffre[1]
     nbDeuxChiffre = dizaine + unité
-
     if nbDeuxChiffre <= 20:
         resultat = chiffreEnLettre[nbDeuxChiffre]
     else:
         resultat = chiffreEnLettre[dizaine] + "-" + chiffreEnLettre[unité]
     return resultat
 
-print(deuxChiffre(nombreEnChiffre))
+#Convertisseur de centaine
+def plusPetitQueMille(nombreEnChiffre):
+    centaineEnChiffre = nombreEnChiffre.pop(0)
+    if centaineEnChiffre == 1:
+        resultat = "cent " + deuxChiffre(nombreEnChiffre)
+    else:
+        centaineEnLettre = chiffreEnLettre[centaineEnChiffre]
+        resultat = centaineEnLettre + " cent " +  deuxChiffre(nombreEnChiffre)
+    return resultat
+
+#Convertisseur de millier 
+def plusPetitQueDixMille(nombreEnChiffre):
+    millierEnChiffre = nombreEnChiffre.pop(0)
+    if millierEnChiffre == 1:
+        resultat = "mille " + plusPetitQueMille(nombreEnChiffre)
+    else:
+        millierEnLettre = chiffreEnLettre[millierEnChiffre]
+        resultat = millierEnLettre + " mille " +  plusPetitQueMille(nombreEnChiffre)
+    return resultat
+
+#Main and accepte en le nombre à convertir 
+userInput = input("Veuillez entré un chiffre : ")
+kata_quatre(userInput)
+
+###################Test Unitaire###################
+# test_number_to_text = {
+# 	1: "un",
+# 	5: "cinq",
+# 	10: "dix",
+# 	18: "dix-huit",
+# 	42: "quarante-deux",
+# 	99: "nonante-neuf",
+#    132: "cent trente-deux",
+#    327: "trois cent vingt-sept",
+#   1337: "mille trois cent trente-sept",
+#   3224: "trois mille deux cent vingt-quatre"
+# }
+  
+# for n, cs in test_number_to_text.items():
+# 	ts = chiffreAText(n)
+# 	if cs == ts:
+# 		print(n, ": OK")
+# 	else:
+# 		print(n, ts, "instead of", cs)
+####################################################
